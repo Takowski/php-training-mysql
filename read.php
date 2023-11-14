051d3f3a69be1330e7a3c0932ac0ee821772397c
Takowski <93830775+Takowski@users.noreply.github.com>
2023-11-14 14:16:33 +0100

    display rando read.php

--- read.php
+++ read.php
@@ -7,8 +7,32 @@
   </head>
   <body>
     <h1>Liste des randonnées</h1>
+    <?php
+    // Connexion à la base de données
+    try
+    {
+    $bdd= new PDO('mysql:host=localhost;dbname=becode;charset=utf8', 'root', 'root');
+    $sql = "SELECT * FROM hiking";
+    $response = $bdd->query($sql);
+    $hikings = $response->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch(Exception $e)
+    {
+    die('Erreur : '.$e->getMessage());
+    }
+    ?>
     <table>
       <!-- Afficher la liste des randonnées -->
+      <?php foreach ($hikings as $hiking): ?>
+        <tr>
+          <td><?php echo htmlspecialchars($hiking['id']); ?></td>
+          <td><?php echo htmlspecialchars($hiking['name']); ?></td>
+          <td><?php echo htmlspecialchars($hiking['difficulty']); ?></td>
+          <td><?php echo htmlspecialchars($hiking['distance']); ?></td>
+          <td><?php echo htmlspecialchars($hiking['duration']); ?></td>
+          <td><?php echo htmlspecialchars($hiking['height_difference']); ?></td>
+        </tr>
+      <?php endforeach; ?>
     </table>
   </body>
 </html>
