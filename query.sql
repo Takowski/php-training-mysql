c3b526c2cee8b438f75d4f9d8ae959cd62a5dfcc
Takowski <93830775+Takowski@users.noreply.github.com>
2023-11-14 13:46:02 +0100

    init

---
+++ query.sql
@@ -0,0 +1,8 @@
+INSERT INTO hiking (id, name, difficulty, distance,duration,height_difference) 
+VALUES 
+(1, 'Aurère par le Sentier Scout depuis le Bélier', 'difficile', '6h30','17.80','1200'),
+(2, 'Boucle des Margosiers depuis le Maïdo', 'difficile', '6h30','17.80','1200'),
+(3,'Du Col des Bœufs à la Nouvelle et retour','moyen','15.1','5h','800'),
+(4,'La Roche Ecrite depuis Mamode Camp','moyen','18,6','6h30','1100'),
+(5,'De la Grande Fontaine au Bassin Sandrine par le Chemin Charrette Bœuf','facile','6.7','2h30','340');
+
