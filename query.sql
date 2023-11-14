cd4fa67ee69e3e93a1237e5473d224f16966c288
Takowski <93830775+Takowski@users.noreply.github.com>
2023-11-14 13:59:29 +0100

    query insert 5 rando

--- query.sql
+++ query.sql
@@ -1,8 +1,7 @@
-INSERT INTO hiking (id, name, difficulty, distance,duration,height_difference) 
+INSERT INTO hiking (id, name, difficulty, distance, duration, height_difference) 
 VALUES 
-(1, 'Aurère par le Sentier Scout depuis le Bélier', 'difficile', '6h30','17.80','1200'),
-(2, 'Boucle des Margosiers depuis le Maïdo', 'difficile', '6h30','17.80','1200'),
-(3,'Du Col des Bœufs à la Nouvelle et retour','moyen','15.1','5h','800'),
-(4,'La Roche Ecrite depuis Mamode Camp','moyen','18,6','6h30','1100'),
-(5,'De la Grande Fontaine au Bassin Sandrine par le Chemin Charrette Bœuf','facile','6.7','2h30','340');
-
+(1, 'Aurère par le Sentier Scout depuis le Bélier', 'difficile', 17.80, '06:30:00', 1200),
+(2, 'La Boucle des Margosiers à la Plaine des Grègues', 'difficile', 11, '05:00:00', 900),
+(3, 'Du Col des Bœufs à la Nouvelle et retour', 'moyen', 15.1, '05:00:00', 800),
+(4, 'La Roche Ecrite depuis Mamode Camp', 'moyen', 18.6, '06:30:00', 1100),
+(5, 'De la Grande Fontaine au Bassin Sandrine par le Chemin Charrette Bœuf', 'facile', 6.7, '02:30:00', 340);
