a592763238fd49e76d65f1b915e5ba922269f2aa
Takowski <93830775+Takowski@users.noreply.github.com>
2023-11-14 14:55:50 +0100

    post rando

--- create.php
+++ create.php
@@ -6,7 +6,25 @@
 	<link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
 </head>
 <body>
-	<a href="/php-pdo/read.php">Liste des données</a>
+	<?php
+	include 'read.php';
+	if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST)) {
+		// Prepare the SQL query
+		$sql = "INSERT INTO hiking (name, difficulty, distance, duration, height_difference) VALUES (:name, :difficulty, :distance, :duration, :height_difference)";
+	
+		// Prepare statement
+		$stmt = $bdd->prepare($sql);
+	
+		// Execute the query with the form data
+		$stmt->execute(array(
+			':name' => $_POST['name'],
+			':difficulty' => $_POST['difficulty'],
+			':distance' => $_POST['distance'],
+			':duration' => $_POST['duration'],
+			':height_difference' => $_POST['height_difference']
+		));
+	}
+	?>
 	<h1>Ajouter</h1>
 	<form action="" method="post">
 		<div>
